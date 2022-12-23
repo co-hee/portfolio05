2ef894560c6c0eea5118ce1012bfb6fdb2597ee3
Seonghee Yun <104363065+intacthee@users.noreply.github.com>
2022-12-23 13:55:12 +0900

    Add files via upload

--- js/portfolio05.js
+++ js/portfolio05.js
@@ -9,19 +9,20 @@ $(function () {
     });
 
 
+    $('.mainContent .cont .tab>div').slick({
+        autoplay: false,
+        slidesToShow: 3,
+        arrows: false,
 
+    });
+
+    $('.mainContent .cont .tab>div').eq(0).slick('slickPlay');
     $('.mainContent .menu_gnb li').on('click', function () {
-
-
         var idx = $(this).index();
         $(this).addClass('on').siblings().removeClass('on');
         $('.mainContent .cont .tab>div').eq(idx).addClass('on').siblings().removeClass('on');
-        $('.mainContent .cont .tab>div').eq(idx).slick({
-            autoplay: false,
-            slidesToShow: 2,
-            arrows: false,
-        });
-
+        $('.mainContent .cont .tab>div').slick('slickPause');
+        $('.mainContent .cont .tab>div').eq(idx).slick('slickPlay');
     })
 
 
